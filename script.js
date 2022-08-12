c035dc66d3dcb8e87ddddbe242943e290807d668
upender03 <111115983+upender03@users.noreply.github.com>
2022-08-12 13:50:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function add (a,b) {
+    return a+b;
+}
+function sub (a,b) {
+    return a-b;
+}
+function mul (a,b) {
+    return a*b;
+}
+function div (a,b) {
+    return a/b;
+}
+function rem (a,b) {
+    return a%b;
+}
+function expo (a,b) {
+    return a**b;
+}
+console.log(add(5,6));
+console.log(sub(4,8));
+console.log(mul(6,7));
+console.log(div(10,5));
+console.log(rem(8,4));
+console.log(expo(6,3));
+
+
+ 
